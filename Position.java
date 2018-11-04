8a4f687db9da6f6ffc6e935d2de000668fc66e9b
Anton Sviridov <asvirido@e1r7p5.unit.ua>
2018-11-04 20:19:21 +0200

    add class Position

---
+++ Position.java
@@ -0,0 +1,25 @@
+public class Position {
+	private int x;
+	private int y;
+
+	public Position(final int x, final int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return (this.x);
+	}
+
+	public int getY() {
+		return (this.y);
+	}
+
+	public void setX(final int x) {
+		this.x = x;
+	}
+
+	public void setY(final int y) {
+		this.y = y;
+	}
+}
